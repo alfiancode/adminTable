e87c5badc511fa168f1675d40a91bd9973242d10
dadoseto900 <dadoseto@outlook.com>
2023-01-15 21:54:10 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import AdminTable from './components/adminTable';
+
+function App() {
+
+
+  return (
+    <>
+      <AdminTable />
+    </>
+  );
+}
+
+export default App
